b013f6d50968f1ab46ea044b6b3ac5cbea218145
Srikantha_Arththika <arththika04@gmail.com>
2025-12-22 20:21:35 +0530

    first

---
+++ studenthide/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <div className="container-fluid">
+        <span className="navbar-brand">Student Attendance</span>
+        <button className="btn btn-light">Add New Attendance</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
